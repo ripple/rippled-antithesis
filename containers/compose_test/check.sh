10c2c71fb9fd5d22f91cc7774ba5066379cc337f
Bronek Kozicki <brok@incorrekt.com>
2024-05-09 18:31:12 +0100

    Add node4 and node5

--- containers/compose_test/check.sh
+++ containers/compose_test/check.sh
@@ -2,6 +2,6 @@
 
 address=$(docker network inspect rippled-net | jq -r .[0].IPAM.Config[0].Gateway)
 address=$(echo "${address%${address##*.}}")
-curl ${address}{2,3,4,5,6}:5005 \
+curl ${address}{2,3,4,5,6,7,8}:5005 \
     --silent \
-    --data '{"method": "server_info"}' | jq -r '.result.info | {hostid, build_version, complete_ledgers, server_state, uptime}'
+    --data '{"method": "server_info"}' | jq -r '.result.info | {hostid, build_version, complete_ledgers, server_state, uptime, peers}'
