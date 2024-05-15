e549bf78b051d268417861953159545cf64db90e
Bronek Kozicki <brok@incorrekt.com>
2024-05-15 14:40:37 +0100

    Add workload and tmp_workload containers

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-address=$(docker network inspect config_antithesis-net | jq -r '.[0].IPAM.Config[0].Subnet')
+address=$(docker network inspect config_rippled-net | jq -r '.[0].IPAM.Config[0].Subnet')
 address=$(echo "${address%${address##*.}}")
-curl ${address}{2,3,4,5,6,7,8}:5005 \
+curl ${address}{3,4,5,6,7,8,9}:5005 \
     --silent \
     --data '{"method": "server_info"}' | jq -r '.result.info | {hostid, build_version, complete_ledgers, server_state, uptime, peers}'
