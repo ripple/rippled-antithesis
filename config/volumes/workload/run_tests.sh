fb39ff8bfd6a0e3050a107426a6af43d9f90076d
Bronek Kozicki <brok@incorrekt.com>
2024-06-27 17:25:14 +0100

    Update images for workload and rippled (#2)
    * More packages in workload
    * Update rippled for PR #5042
    * Query all validators
    * Add config
    * Update image tags

--- config/volumes/workload/run_tests.sh
+++ config/volumes/workload/run_tests.sh
@@ -2,7 +2,16 @@
 
 cd auto
 
-for i in {10..1}; do echo "waiting ... $i" && sleep 10s ; done
+echo "Waiting for $@"
+DONE=0
+while [ $DONE -eq 0 ]; do
+  DONE=1
+  for i in $@; do
+    curl --silent --data '{"method": "server_info"}' http://$i:5005/ | jq -r '.result.info.server_state' | grep -E "^proposing$|^full$" >/dev/null || DONE=0
+  done
+  [ $DONE -eq 1 ] || echo "waiting ..."
+  sleep 5s
+done
 
 # The framework is hard-coded to try to run on a real network so replace the actual devnet test fund account with the real genesis account
 sed -i 's/rh1HPuRVsYYvThxG2Bs1MfjmrVC73S16Fb/rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh/' ./rippled_automation/rippled_end_to_end_scenarios/end_to_end_tests/constants.py
